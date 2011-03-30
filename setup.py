73aae5d0f0dc8da5ab3e3f48755b726b2d065a51
Ian Halpern <ian@ian-halpern.com>
2011-03-30 02:18:35 -0400

    * initial commit of zimr.handlers.wsgi

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+	name         = 'zimr.handlers.wsgi',
+	version      = '0.1.0',
+	description  = 'A wsgi handler for zimr',
+	author       = 'Ian Halpern',
+	author_email = 'ian@ian-halpern.com',
+	url          = '',
+	download_url = '',
+	py_modules   = ( 'zimr.handlers.wsgi', )
+)
+
---
+++ zimr/handlers/wsgi.py
@@ -0,0 +1,65 @@
+import zimr, urllib
+from urlparse import urlparse
+from StringIO import StringIO
+
+class ZimrLogIO( StringIO ):
+
+	def flush( self ):
+		zimr.log( self.getValue() )
+		return cStringIO.flush( self )
+
+def call_application( app, environ ):
+	body = []
+	status_headers = [None, None]
+
+	def start_response(status, headers):
+		status_headers[:] = [status, headers]
+		return body.append
+
+	app_iter = app(environ, start_response)
+
+	try:
+		for item in app_iter:
+			body.append(item)
+	finally:
+		if hasattr(app_iter, 'close'):
+			app_iter.close()
+
+	return status_headers[0], status_headers[1], ''.join(body)
+
+def connection_handler(  application, connection ):
+	server = urlparse( connection.website.protocol + connection.website.url )
+
+	environ = {
+		'REQUEST_METHOD': connection.request.method,
+		'SCRIPT_NAME': server.path if server.path != '/' else '',
+		'PATH_INFO': '/' + connection.request.url if connection.request.url else '',
+		'QUERY_STRING': '&'.join([k+'='+urllib.quote(str(v)) for (k,v) in connection.request.params.items()]),
+		'CONTENT_TYPE': connection.request.headers['Content-Type'],
+		'CONTENT_LENGTH': connection.request.headers['Content-Length'],
+		'SERVER_NAME': server.hostname,
+		'SERVER_PORT': server.port if server.port else ( 443 if server.scheme == 'https' else 80 ),
+		'SERVER_PROTOCOL': 'HTTP/1.1',
+		'wsgi.version': (1,0),
+		'wsgi.url_scheme': server.scheme,
+		'wsgi.input': StringIO( connection.request.post_body ),
+		'wsgi.errors': ZimrLogIO(),
+		'wsgi.multithread': False,
+		'wsgi.multiprocess': False,
+		'wsgi.run_once': False
+	}
+
+	if server.scheme == 'https':
+		environ['HTTPS'] = 'on'
+
+	for name in connection.request.headers.keys():
+		environ['HTTP_' + name.upper()] = connection.request.headers[name]
+
+	status, headers, body = call_application( application, environ )
+
+	connection.response.setStatus( int(status[:3]) )
+
+	for name, val in headers:
+		connection.response.headers[ name ] = val
+
+	connection.send( body )
